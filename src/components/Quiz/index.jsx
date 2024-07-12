78c502b7fc1c03751b7dd2c889e3b9acad444006
Thiago-Ferreira <thiagocraft510@gmail.com>
2024-07-12 01:44:43 -0300

    Adciona a possibilidade de colocar imagens no quiz, e outros tipos de questões(checkbox e campo de resposta)

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -3,49 +3,100 @@ import {
     VStack,
     Box,
     Button,
-    Badge
+    Badge,
+    Image
  } from '@chakra-ui/react'
 import { useState } from 'react';
 
+
+/**
+ * 
+ * @param {object[]} questions - Array de questões
+ * @param {string} questions[].question - Pergunta da questão
+ * @param {object[]} question[].options - Opções da questão
+ * @returns 
+ */
 export function Quiz({questions}){
-    console.log(questions);
 
     const [currentQuestion, setCurrentQuestion] = useState(1)
     const [score, setScore] = useState(0)
-
+    //history 
     function showNextQuestion(){
         setTimeout( ()=> setCurrentQuestion(currentQuestion + 1), 100)
     }
 
     function submitQuestion(e){
+        //depende do tipo da questão
         e.preventDefault()
         let data = new FormData(e.currentTarget)
 
         let userResponse = data.get('options')
         let optionSelectedForUser = questions[currentQuestion - 1].options.find( option => option.content === userResponse )
-        console.log(optionSelectedForUser);
         if(optionSelectedForUser.isTheCorrect){
             setScore(score+1)
+        }
+
+        e.currentTarget.reset()
+        // o formulario se alimpa
+    }
+    function renderOptionsFromQuestionType(questionType, options) {
+        console.log('o que chegou no render',questionType, options);
+        
+        switch (questionType) {
+            case "UniqueItem":
+                return options.map( (option, index) => {
+                    console.log(option);
+                        return(
+                            <div key={index} style={{display:'flex', alignItems:'baseline',gap: "0.5rem"}}>
+                                <input type='radio' style={{width:'fit-content'}} name='options' value={option.content} key={index}/>
+                                <p style={{textAlign: 'left'}}>{option.content}</p>
+                            </div>
+                        )
+                } )
+
+            case "MultipleItems":
+                return options.map( (option, index) => {
+                    return(
+                        <div key={index} style={{display:'flex', alignItems:'baseline',gap: "0.5rem"}}>
+                            <input type='checkbox' style={{width:'fit-content'}} name='options' value={option.content} key={index}/>
+                            <p style={{textAlign: 'left'}}>{option.content}</p>
+                        </div>
+                    )
+                } )
+
+                case "InputQuestion":
+                    
+                    return options.map( (option, index) => {
+                        console.log(option);
+                        return(
+                            <div key={index} style={{display:'flex', alignItems:'baseline',gap: "0.5rem"}}>
+                                <p>{option.InputField}</p>
+                                <input type='text' placeholder={option.InputField} style={{width:'fit-content'}} name='options' value={option.content} key={index}/>
+                            </div>
+                        )
+                    } )
+        
+            default:
+                return "Deu ruim!!!"
         }
     }
 
 
+    console.log(questions[currentQuestion - 1].questionType, questions[currentQuestion - 1]);
+    //erro na hora de passar a questão era imprimido dois tipos sendo um da questão anterior e outro da atual
     return(
-        <form onSubmit={submitQuestion} >
+        <form onSubmit={submitQuestion} style={{marginInline: 'auto'}}>
             <Box p='10' position='relative'>
-                <Badge position="absolute" top="1rem" right="1rem" py='0.125rem' px='0.5rem'>{`${score}/${questions.length}`}</Badge>
+                <Badge position="absolute" top="1rem" right="1rem" py='0.125rem' px='0.5rem'>{`${currentQuestion}/${questions.length}`}</Badge>
                 <Heading size='md' mb='1.5rem'>{questions[currentQuestion - 1].question}</Heading>
+                
+                {questions[currentQuestion - 1].image == undefined || (
+                    <Image src={questions[currentQuestion - 1].image} fallbackSrc='https://via.placeholder.com/150' mx='auto' my='0.5rem' />
+                ) }
                 
                     <VStack spacing='24px' alignItems='flex-start'>
                         {
-                            questions[currentQuestion - 1].options.map( (option, index) => {
-                                return(
-                                    <div key={index} style={{display:'flex', alignItems:'center',gap: "0.5rem"}}>
-                                        <input type='radio' style={{width:'fit-content'}} name='options' value={option.content} key={index}/>
-                                        <p>{option.content}</p>
-                                    </div>
-                                )
-                            } )
+                            renderOptionsFromQuestionType(questions[currentQuestion - 1].questionType, questions[currentQuestion - 1].options)
                         }
                     </VStack>
             </Box>
