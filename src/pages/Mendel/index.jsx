e78f65d8fe7c007df10c7b02f9e3f6cef0dd0901
AlessandroSilvaS <alessandrosoares1409@gmail.com>
2024-07-15 18:23:09 -0300

    15/07/2024
    Criação da pagina mendel e da sua respectiva rota, assim como as correções de conclitos

---
+++ src/pages/Mendel/index.jsx
@@ -0,0 +1,11 @@
+import { Biography } from "../Biography";
+import { Curiosities } from "../Curiosities"
+
+export function Mendel(){
+    return(
+        <div className="mendel">
+            <Biography/>
+            <Curiosities />
+        </div>
+    )
+}
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Alelos } from '../pages/Alelos';
 import { Puner } from '../pages/Puner';
+import { Mendel } from '../pages/Mendel';
 
 export const RouterAsRouter = () => {
   return (
@@ -12,6 +13,7 @@ export const RouterAsRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/alelos" element={<Alelos />} />
         <Route path="/puner" element={<Puner />} />
+        <Route path="/mendel" element={<Mendel />} />
       </Routes>
     </BrowserRouter>
   );
