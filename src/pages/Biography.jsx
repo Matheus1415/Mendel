dc873078494ea15c2dbc7cfb986b6b70ae8f6034
AlessandroSilvaS <alessandrosoares1409@gmail.com>
2024-07-15 16:39:08 -0300

    15/07/2024

--- src/pages/Biography.jsx
+++ src/pages/Biography.jsx
@@ -1,26 +1,20 @@
-import { Flex, Heading, Highlight, Image, Text  } from "@chakra-ui/react";
+import { Box, Flex, Heading, Highlight, Image, Text  } from "@chakra-ui/react";
 import '../themes/biography.css'
-import '../themes/response.css'
 import GregorMendelImage from '../images/Gregor_Mendel.jpg'
 import GregorNotesImage from '../images/Anotacoes_Mendel.jpg'
-import { MdOpacity } from "react-icons/md";
 
 export function Biography() {
   return (
-  <Flex aling="Center" direction="column">
+  <Flex aling="Center" direction="column" maxWidth='1438'>
 
-{/*Title*/}
-
-    <Heading size='4xl' marginLeft="5%" id="Title">
-      <Highlight query='Mendel' styles={{color: "#00FF7F"}}>
-        Quem foi Mendel ?
-      </Highlight> 
-    </Heading>
-
-{/* Pages's content */}
-
-    <Flex aling="center" direction="Row" justify='space-between' padding="24px 24px" marginTop="20px" id="Container">
-        <Text fontSize= '2xl' maxWidth = {600} marginLeft="5%" textAlign='justify' aling="center" marginTop="18px" fontWeight='700' id="Bio"> 
+    <Flex aling="center" direction={["column-reverse", "row"]} justify={['start', 'space-between']} padding="24px 24px" marginTop={['10px', '20px']} id="Container">
+      <Box>
+        <Heading fontSize='40px' marginLeft="5%" id="Title" top='111px' left='178pxs'  fontWeight='275' lineHeight='60px'>
+          <Highlight query='Biografia' styles={{color: "#00FF7F"}}>
+            BIOGRAFIA
+          </Highlight> 
+        </Heading>
+        <Text fontSize={['md', '2xl', '2xl', '2xl']} w = {[300, 600]} marginLeft="5%" textAlign='justify' aling="center" marginTop="0px" fontWeight='700' id="Bio"> 
           Nascido no Império Austro-Húngaro (hoje República Tcheca), foi um
           professor, botânico e prelado agostiniano, primeiro cientista a
           modelar o conceito de hereditariedade, complementando a Teoria de
@@ -32,21 +26,23 @@ export function Biography() {
           Olmütz (Olomouc, República Tcheca), onde se destacou em física e
           matemática, concluindo seus estudos em 1843.
         </Text>
+      </Box>
 
 {/* Page's images */}
-        <Flex position="relative" marginRight="5%" id="Image_area" marginLeft="5%">
+        <Box position="relative" marginRight="5%" id="Image_area" marginLeft="5%">
           <Image
             id="Image_mendel"
             src={GregorMendelImage}
             alt='Imagem do Mendel'
-            borderRadius='50%' />
+            boxSize={[300, 500]} />
 
           <Image
             id="Image_anot"
             src={GregorNotesImage}
-            alt='Imagem anotações'/>
+            alt='Imagem anotações'
+            boxSize={[300, 500]}/>
 
-        </Flex>
+        </Box>
     </Flex>
   </Flex>
   )
