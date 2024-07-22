182904545635092a625d1f62eeccc29fccd2ce38
Daniel de Sousa <156535206+DanielDeSousaDEV@users.noreply.github.com>
2024-07-22 14:16:52 -0300

    Update index.jsx

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -3,23 +3,21 @@ import {
     VStack,
     Box,
     Button,
+    Text,
     Badge,
     Image,
     FormControl,
     FormLabel,
     Input,
-    IconButton,
-    Icon,
-    HStack,
     Checkbox,
     Radio,
     RadioGroup,
-    Stack
+    CheckboxGroup,
+    Flex
  } from '@chakra-ui/react'
+ import { motion } from 'framer-motion';
 import { useState } from 'react';
-import { FaArrowRight } from 'react-icons/fa';
-import { IoArrowForwardCircle } from 'react-icons/io5';
-
+import { submitForm } from '../../config/submitForm.js'
 
 /**
  * 
@@ -31,96 +29,191 @@ import { IoArrowForwardCircle } from 'react-icons/io5';
 export function Quiz({questions}){
 
     const [currentQuestion, setCurrentQuestion] = useState(1)
-    const [score, setScore] = useState(0)
+    const [winningHistory, setWinningHistory] = useState([])
     const [showResults, setShowResults] = useState(false)
-    //history 
+    const [zoomedImage, setZoomedImage] = useState(false)
+
+    const zoomVariants = {
+        //aqui vão as estilizações que devem ser feitas com base no state zoomedImage
+        image: {
+            initial: {scale: 1.0},
+            zoomed: {transform: 'scale(1.2;1.2)', position:'fixed' , top:'50%',left:'50%',transform: 'translate(-50%,-50%)', zIndex: '10000'}
+        },
+        div: {
+            initial: {opacity: 0, display: 'none'},
+            zoomed: {
+                //alou
+            }
+        }
+    }    
+
     function showNextQuestion(){
-        setTimeout( ()=> setCurrentQuestion(currentQuestion + 1), 100)
+        if (currentQuestion >= questions.length) {
+            setShowResults(true)
+            console.log('chegou no final', winningHistory);
+            return;
+        }
+        console.log(`da questão ${currentQuestion} -> ${currentQuestion + 1} | \n Resultado:`,winningHistory);
+
+        setTimeout( ()=> setCurrentQuestion(currentQuestion + 1), 150)
     }
 
+    function toggleZoomImage(){
+        setZoomedImage(!zoomedImage)
+        console.log(`zoom ${zoomedImage} -> ${!zoomedImage}`);
+    }
+
+    //enviar essa função para o config
     function submitQuestion(e){
         e.preventDefault()
-        // ToDo - depende do tipo da questão
-
+    
         const data = new FormData(e.currentTarget)
+        console.log("---Valores do Form Data---");
         for (const [name,value] of data) {
             console.log(name, ":", value)
         }
         console.log( questions[currentQuestion - 1]);
-
+        console.log("---/Valores do Form Data---");
+    
+    
         switch (questions[currentQuestion - 1].questionType) {
-            case 'UniqueItem':
+            case 'UniqueItem':    
+                console.log('submit questão de item unico');
                 let selectedOptionForUser = questions[currentQuestion - 1].options.find( option => {
                     return data.get('options') === option.content
                 } )
-
-                console.log('selected:',selectedOptionForUser);
-
+    
                 if(selectedOptionForUser.isCorrect === true){
-                    alert('parabens')
+                    console.log('unique certo');
+                    setWinningHistory( [...winningHistory, {
+                        Question: currentQuestion,
+                        answeredCorrectly: true
+                    }] )
                 }else{
-                    alert('errado')
+                    console.log('unique errado');
+                    setWinningHistory( [...winningHistory, {
+                        Question: currentQuestion,
+                        answeredCorrectly: false
+                    }] )
                 }
                 showNextQuestion()
                 break;
-
+    
             case 'MultipleItems':
-                console.log('chegou nos multiplos items');
-                console.log(data.get('Sim'));
-                let a = questions[currentQuestion - 1].options.map( option => {
-                    return data.get(option.content)
+                    
+                console.log('submit questão de item Multiplos');
+                let selectedOptions = []
+    
+                for( let key of data.keys() ){
+                    selectedOptions.push(key)
+                }
+    
+                let correctQuestionOptions = questions[currentQuestion - 1].options.filter( option => {
+                    return option.isCorrect
+                })
+    
+                let isAllOptionsSelectedAreCorrect = selectedOptions.every( selectedOption => {
+                    const anyCorrectOptionCorrespondsToTheSelectedItem = correctQuestionOptions.some( correctOption => correctOption.content === selectedOption )
+                    return anyCorrectOptionCorrespondsToTheSelectedItem
                 } )
-                console.log(a);
-
-                console.log('submit questão de item multiplos');
+    
+                if( (isAllOptionsSelectedAreCorrect) && (selectedOptions.length === correctQuestionOptions.length) ){
+                    setWinningHistory([...winningHistory, {
+                        Question: currentQuestion,
+                        answeredCorrectly: true
+                    }])
+                }else{
+                    setWinningHistory([...winningHistory, {
+                        Question: currentQuestion,
+                        answeredCorrectly: false
+                    }])
+                }
                 showNextQuestion()
                 break;
-
+    
             case 'InputQuestion':
                 console.log('submit questão de input');
+                //lista booleanificada de valores que o usuario digitou correto
+                const listOfCertainMarkedValues = [];
+    
+                for (const [name,value] of data) {
+                    const currentInputField = questions[currentQuestion - 1].options.find( option => option.InputField === name )
+    
+                    const userEnteredCorrectValue = value === currentInputField.isCorrectValue
+    
+                    listOfCertainMarkedValues.push(userEnteredCorrectValue)
+                }
+    
+                const userEnteredAllValuesCorrectly = listOfCertainMarkedValues.every( value => value === true )
+                console.log('Usuario digitou todos valores corretamente: ',userEnteredAllValuesCorrectly);
+    
+                console.log('lista de valores booleanificados: ',listOfCertainMarkedValues);
+                if(userEnteredAllValuesCorrectly){
+                    console.log('input certo');
+                    setWinningHistory( [...winningHistory, {
+                        Question: currentQuestion,
+                        answeredCorrectly: true
+                    }] )
+                }else{
+                    console.log('input errado');
+                    setWinningHistory( [...winningHistory, {
+                        Question: currentQuestion,
+                        answeredCorrectly: false
+                    }] )
+                }
+                showNextQuestion()
                 break;
         
             default:
+    
                 console.log('submit invalido');
                 break;
             
         }
-
-
+        
+    
         //e.currentTarget.reset()
         // o formulario se alimpa
     }
+
+    function resetQuiz(){
+        setWinningHistory([])
+        setCurrentQuestion(1)
+        setShowResults(false)
+    }
+
     function renderOptionsFromQuestionType(questionType, options) {
         
         switch (questionType) {
             case "UniqueItem":
-                let optionsJsx = options.map( (option, index) => {
+                const radiosJsx = options.map( (option, index) => {
                         return(
-                            <Radio w='full' value={option.content} name='options'>{option.content}</Radio>
+                            <Radio w='full' key={index} value={option.content} name='options'>{option.content}</Radio>
                         )
                 } )
                 //key para o radio group
                 // required
                 return (
                     <RadioGroup name='options' display='flex' flexDirection='column' gap='24px' alignItems='flex-start' w='full'>
-                        {optionsJsx}
+                        {radiosJsx}
                     </RadioGroup>
                 )
 
             case "MultipleItems":
                 // chackra injeta value nos checkboxes por default
-                return options.map( (option, index) => {
-                    return(
-                        <HStack p='0.25rem' spacing='2px' w='full' key={index}>
-                            <Checkbox flex='1' value={option.content} name={option.content}>{option.content}</Checkbox>
-                        </HStack>
-                    )
+                const checkboxesJsx = options.map( (option, index) => {
+                    return <Checkbox w='full' key={index} name={option.content} >{option.content}</Checkbox>
                 } )
+
+                return (
+                    <CheckboxGroup p='0.25rem' spacing='2px' w='full' alignItems='center'>
+                        {checkboxesJsx}
+                    </CheckboxGroup>
+                )
 
                 case "InputQuestion":
                     
                     return options.map( (option, index) => {
-                        console.log(option.InputField);
                         return(
                             <FormControl key={index} isRequired display='flex' alignItems='center' justifyContent='space-between'>
                                 <FormLabel m='0' p='0.75rem' w='fit-content'>{option.InputField}:</FormLabel>
@@ -130,39 +223,83 @@ export function Quiz({questions}){
                     } )
         
             default:
-                return "Deu ruim!!!"
+                return console.warn('Nâo foi possivel renderizar as Opções')
         }
     }
-    function zoomImage(e) {
-        e.target.style.position = 'absolute'
-        e.target.style.zIndex = '100'
-        e.target.style.top = '0'
-        e.target.style.bottom = '0'
-        e.target.style.left = '0'
-        e.target.style.right = '0'
-        //pai dele é relativo, talvez tenha que usar State
-    }
+
+    
+    let amountOfWins = winningHistory.filter( issueHistory => issueHistory.answeredCorrectly ).length
+    let hitPercentage = ((amountOfWins / questions.length).toFixed(2)) * 100
+
 
     //erro na hora de passar a questão era imprimido dois tipos sendo um da questão anterior e outro da atual
     return(
-        <Box mx='auto' backgroundColor='lightgray' minW='50%' maxW='80%' w='fit-content'>
-            <form onSubmit={submitQuestion} style={{ padding: '2.5rem', position:'relative' }} >
-            
-
-                <Badge position="absolute" top="1rem" right="1rem" py='0.125rem' px='0.5rem'>{`${currentQuestion}/${questions.length}`}</Badge>
-                <Heading size='md' mb='1.5rem'>{questions[currentQuestion - 1].question}</Heading>
-                
-                {questions[currentQuestion - 1].image == undefined || (
-                    <Image src={questions[currentQuestion - 1].image} fallbackSrc='https://via.placeholder.com/150' mx='auto' my='0.5rem' borderRadius='xl' transition='0.3s' _hover={{transform: 'scale(0.95)'}} />
-                ) }
-                
-                    <VStack spacing='24px' alignItems='flex-start'>
-                        {
-                            renderOptionsFromQuestionType(questions[currentQuestion - 1].questionType, questions[currentQuestion - 1].options)
-                        }
-                    </VStack>
-                    <Button type='submit' display='flex' alignItems='center' gap='0.5rem' w='full' color='white' bg='teal.500' _hover={{bg: 'teal.600'}} >Enviar Respostas</Button>
-            </form>
+        <Box mx='auto' backgroundColor='lightgray' minW='50%' maxW='80%' w='fit-content' borderRadius='xl' overflow='hidden'>
+            {
+                !showResults ? (
+                    <form onSubmit={submitQuestion} style={{ padding: '2.5rem', position:'relative', margin: 0, backgroundColor:'transparent', borderRadius:0 }} >
+                        <Badge position="absolute" top="1rem" right="1rem" py='0.125rem' px='0.5rem'>{`${currentQuestion}/${questions.length}`}</Badge>
+                        <Heading size='md' mb='1.5rem'>{questions[currentQuestion - 1].question}</Heading>
+                        
+                        {questions[currentQuestion - 1].image && (
+                            <>
+                                {zoomedImage && (
+                                    <>
+                                        <Box 
+                                            as={motion.div} 
+                                            variants={zoomVariants.div} 
+                                            animate={zoomedImage ? 'zoomed' : 'initial' }
+                                            position='fixed'
+                                            top='0'
+                                            left='0'
+                                            right='0'
+                                            bottom='0'
+                                            opacity='0.8'
+                                            backgroundColor='red'
+                                            zIndex='10000'
+                                            //position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, opacity: 0.4, backgroundColor: 'red'
+                                        >
+                                        </Box>
+                                        <Image 
+                                            src={questions[currentQuestion - 1].image}
+                                            fallbackSrc='https://via.placeholder.com/150'
+                                            borderRadius='xl'
+                                            mx='auto'
+                                            visibility={zoomedImage ? true : false}
+                                        />
+                                        
+                                    </>
+                                )}
+                                <Image 
+                                    src={questions[currentQuestion - 1].image} 
+                                    onClick={toggleZoomImage}
+                                    variants={zoomVariants.image}
+                                    style={zoomedImage ? zoomVariants.image.zoomed : zoomVariants.image.initial }
+                                    fallbackSrc='https://via.placeholder.com/150'
+                                    borderRadius='xl'
+                                    mx='auto'
+                                />
+                            </>
+                        ) }
+                        
+                            <VStack spacing='24px' alignItems='flex-start'>
+                                {
+                                    renderOptionsFromQuestionType(questions[currentQuestion - 1].questionType, questions[currentQuestion - 1].options)
+                                }
+                            </VStack>
+                            <Button type='submit' position='block' display='flex' alignItems='center' gap='0.5rem' w='full' color='white' bg='teal.500' _hover={{bg: 'teal.600'}} >Enviar Respostas</Button>
+                    </form>
+                ) : (
+                    <Box p='10' display='flex' flexDirection='column' alignItems='center' gap='0.75rem'>
+                        <Heading textColor={hitPercentage < 40 ? 'red' : 'green'}>Pontuação: {amountOfWins} | ({hitPercentage})%</Heading>
+                        <Text>Você acertou {amountOfWins} de {questions.length} questões.</Text>
+                        <Flex direction='row' gap='0.25rem'>
+                            <Button onClick={resetQuiz}>Reiniciar Questionario</Button>
+                            <Button onClick={()=> console.log(winningHistory)}>Imprimir O history</Button>
+                        </Flex>
+                    </Box>
+                )
+            }
             
         </Box>
     )
