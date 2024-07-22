392553c046c9d935e1f65bbe4710475dd4323864
Daniel de Sousa <156535206+DanielDeSousaDEV@users.noreply.github.com>
2024-07-22 14:21:57 -0300

    Update Router.jsx with a new route for Chromosomes's page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Alelos } from '../pages/Alelos';
+import { Chromosomes } from '../pages/Chromosomes';
 
 export const RouterAsRouter = () => {
   return (
     <BrowserRouter>
-        {/*Colocar Menu lateral*/}
+      {/*Colocar Menu lateral*/}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Alelos" element={<Alelos />} />
+        <Route path='/Chromosomes' element={<Chromosomes />}/>
       </Routes>
     </BrowserRouter>
   );
