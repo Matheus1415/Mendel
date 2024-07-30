211927c22fd71765cf4198a1e2ed4e6b4ec23f5d
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-07-30 01:45:04 -0300

    Testando componentes

---
+++ src/components/Teste/index.jsx
@@ -0,0 +1,9 @@
+import { Flex, Text, Container, Box, Image } from "@chakra-ui/react"
+function Teste() {
+    return(
+        <Container>
+            <Text as="h1" TextAling="center">Teste</Text>
+        </Container>
+    )
+}
+export default Teste;
--- src/pages/FatorRh/index.jsx
+++ src/pages/FatorRh/index.jsx
@@ -1,16 +1,9 @@
 import { Flex, Text, Container, Box, Image } from "@chakra-ui/react"
+import Teste from '../../components/Teste';
+
+
 export const FatorRh = () => {
     return(
-        <Flex>
-           <Container maxWidth='4x1' >
-                <Box as="section" h='150px'>
-                    <Text variant='h1' textAlign='center' color='Terciario'>Fator RH</Text>
-                </Box>
-                <Box as="section">
-                    <Text variant='h3' textAlign='right' color='Terciario'>O que é fator Rh?</Text>
-                    <Text variant='p' textAlign='justify' marginRight='0' marginLeft='auto'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas nihil consectetur fugit sequi quam doloribus, velit facere quo ratione culpa esse ea odio minima architecto pariatur fugiat asperiores odit neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sit saepe repellendus quam amet adipisci magnam autem alias! Natus, quam at molestias rerum amet deserunt mollitia distinctio deleniti? Adipisci, reprehenderit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati accusantium, laborum harum enim sapiente et unde iure est ullam deleniti quisquam impedit ducimus nesciunt, accusamus id veniam a dolore?</Text>
-                </Box>
-           </Container>
-        </Flex>
+        <Teste/>
     )
 }
