54e0adc662af062ca8527071750a55969393eecc
Gabriel De Sousa <156588121+Gabriel-Sousa-Dev@users.noreply.github.com>
2024-07-31 15:29:38 -0300

    Update Heading.js

--- src/theme/components/Heading.js
+++ src/theme/components/Heading.js
@@ -3,7 +3,11 @@ import { defineStyle, defineStyleConfig } from "@chakra-ui/react";
 const PrimaryTitle = defineStyle({
     mb: 5,
     fontSize: 40,
-    fontWeight: 200,
+    fontWeight: 275,
+    fontWeight: 'thin',
+    textTransform: 'uppercase',
+    bg: 'linear-gradient(to right, #19BFCD 0%, #0D6067 100%)', //Essas duas propriedades não funcionam se mandadas pela Variant para funcionar basta copiar-las e colar no Heading em questão
+    bgClip: 'text',
     //falta deixar o heading em gradiente
 })
 
