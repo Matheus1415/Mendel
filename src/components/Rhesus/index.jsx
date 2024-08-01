90ea91b2c2cfea25137698b884d1d61479bb7aa1
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-08-01 00:32:07 -0300

    tentando configurar alguns componentes

--- src/components/Rhesus/index.jsx
+++ src/components/Rhesus/index.jsx
@@ -1,24 +1,23 @@
-import { Container, Text, Box, Heading, Image } from '@chakra-ui/react';
+import { Container, Text, Box, Heading} from '@chakra-ui/react';
 export const Rhesus = () => {
 
-
     return(
-       <Container maxWidth="4x1" p="10px">
-            <Box>
-                <Text variant="h1" textAlign="center" color="Terciario" fontWeight="ExtraLight" fontSize="40px">Fator Rh</Text>
+       <Container maxWidth="4x1" m="0px" p="0px">
+            <Box height="200px" bg="#02161F">
+                <Text variant="h1" textAlign="center" color="Terciario" fontWeight="ExtraLight" fontSize="40px" bgImage="url(./img/background1.png) ">Fator Rh</Text>
             </Box>
             <Box>
                 <Heading as="h2" color="Terciario" fontWeight="ExtraLight"  fontSize="30px">O que é Fator Rh?</Heading>
                 
-                <Text fontWeight="ExtraLight" fontSize="17px">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque autem modi praesentium nam? Commodi optio, dolore perferendis facilis voluptas aut inventore illum. At dicta optio iure neque totam repudiandae illum?
+                <Text as="p" fontWeight="ExtraLight" fontSize="17px">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque autem modi praesentium nam? Commodi optio, dolore perferendis facilis voluptas aut inventore illum. At dicta optio iure neque totam repudiandae illum?
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis sit tenetur, cumque distinctio beatae vero ipsum sed laboriosam non eum accusantium excepturi alias velit aut eos et modi dolor. Praesentium.
                 </Text>
-                <Heading as="h2" color="Terciario">Qual a sua importância na tipagem sanguínea?</Heading>
-                <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque autem modi praesentium nam? Commodi optio, dolore perferendis facilis voluptas aut inventore illum. At dicta optio iure neque totam repudiandae illum?
+                <Heading as="h2" color="Terciario" fontWeight="ExtraLight"  fontSize="30px">Qual a sua importância na tipagem sanguínea?</Heading>
+                <Text as="p" fontWeight="ExtraLight" fontSize="17px">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque autem modi praesentium nam? Commodi optio, dolore perferendis facilis voluptas aut inventore illum. At dicta optio iure neque totam repudiandae illum?
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis sit tenetur, cumque distinctio beatae vero ipsum sed laboriosam non eum accusantium excepturi alias velit aut eos et modi dolor. Praesentium.
                 </Text>
-                <Heading as="h2" color="Terciario">Como ele pode influenciar transfusões de sangue e a saúde durante a gravidez?</Heading>
-                <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque autem modi praesentium nam? Commodi optio, dolore perferendis facilis voluptas aut inventore illum. At dicta optio iure neque totam repudiandae illum?
+                <Heading as="h2" color="Terciario" fontWeight="ExtraLight"  fontSize="30px">Como ele pode influenciar transfusões de sangue e a saúde durante a gravidez?</Heading>
+                <Text as="p" fontWeight="ExtraLight" fontSize="17px">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque autem modi praesentium nam? Commodi optio, dolore perferendis facilis voluptas aut inventore illum. At dicta optio iure neque totam repudiandae illum?
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis sit tenetur, cumque distinctio beatae vero ipsum sed laboriosam non eum accusantium excepturi alias velit aut eos et modi dolor. Praesentium.
                 </Text>
             </Box>
--- src/pages/FatorRh/index.jsx
+++ src/pages/FatorRh/index.jsx
@@ -2,7 +2,7 @@ import { Container} from '@chakra-ui/react';
 import Rhesus from "../../components/Rhesus";
 export const FatorRh = () => {
     return(
-        <Container maxWidth="4x1">
+        <Container maxWidth="4x1" m="0px" p="0px">
             <Rhesus />
         </Container>
        
