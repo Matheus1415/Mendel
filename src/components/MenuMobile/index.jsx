5a299460e78e41052eba9f06a9f9375b15574f59
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-08-12 13:04:09 -0300

    inserção de conteudos

--- src/components/MenuMobile/index.jsx
+++ src/components/MenuMobile/index.jsx
@@ -10,6 +10,7 @@ const MenuMobile = ({ isOpen, toggleMenu }) => {
         { label: "Fundamentos", router: "/manutencao" },
         { label: "Introdução a Genética", router: "/manutencao" },
         { label: "Sistema Sanguíneo", router: "/manutencao" },
+        { label: "FatorRh", router: "/FatorRh"}
     ];
 
 
