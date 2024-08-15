0baa728dea1aa0dad4a71f475cf07476e2742a97
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-08-15 10:52:00 -0300

    Testando as props

--- src/components/MendelPage/BiographyFatorRh.jsx
+++ src/components/MendelPage/BiographyFatorRh.jsx
@@ -1,11 +1,11 @@
-import {Text, Container, Box} from '@chakra-ui/react';
+import {Text, Container, Box, Heading} from '@chakra-ui/react';
 import { LayoutBaseModify } from '../../layout/LayoutBaseModify';
 
 
-const BiographyFatorRh = () => {
+const BiographyFatorRh = ({Teste}) => {
     return(
         < LayoutBaseModify>
-                    
+                    <Heading variant="PrimaryTitle"> {Teste} </Heading>
         </LayoutBaseModify>
     )
 }
