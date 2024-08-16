f1956f6932daff5c5ce08190ef0ddaabc18f78c5
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-08-16 13:03:19 -0300

    Inserção de imagens de biografia

--- src/pages/FatorRh/BiographyFatorRh.jsx
+++ src/pages/FatorRh/BiographyFatorRh.jsx
@@ -1,7 +1,7 @@
 import {Heading, Text, Flex, Box} from '@chakra-ui/react';
 import { LayoutBaseModify } from '../../layout/LayoutBaseModify';
 
-const BiographyFatorRh = ({Url, PosicaoDoLado}) => {
+const BiographyFatorRh = ({Name, Url, PosicaoDoLado}) => {
    
 
     return(
@@ -13,7 +13,7 @@ const BiographyFatorRh = ({Url, PosicaoDoLado}) => {
 
             <Flex flex="1" direction="row" justify="space-between" gap={6}>
                 <Box mr="5">
-                    <Heading variant="PrimaryTitle">Karl Landsteiner </Heading>
+                    <Heading variant="PrimaryTitle"> {Name} </Heading>
                     <Text variant="Paragraph">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum eum delectus fugiat quia porro rerum ut quas dolores esse necessitatibus sunt accusantium quisquam repellendus sit velit assumenda, blanditiis quo ratione. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis fuga totam tenetur dolorem explicabo nemo, iusto earum odit aperiam dolorum aut praesentium accusantium deleniti temporibus eaque quo, similique rerum atque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit id ut aspernatur doloremque aperiam iure excepturi quo sit repudiandae commodi pariatur ab, illum est tenetur fugit reiciendis ratione eligendi repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid est, suscipit quod, corporis a praesentium, sed mollitia id corrupti hic quam. Labore nihil accusamus nulla provident atque iusto eius facere? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo, minus error. Praesentium tenetur ut, saepe esse eos ipsa labore, ipsum explicabo quae soluta amet cumque aperiam suscipit optio perferendis corporis.
                     </Text>
--- src/pages/FatorRh/index.jsx
+++ src/pages/FatorRh/index.jsx
@@ -14,7 +14,7 @@ export const FatorRh = () => {
             <LayoutBaseModify
             isRowReverse
             isImage
-            isImageSrc="../public/img/3d-representation-dna.png"
+            isImageSrc="../public/img/DNA.png"
             >
                 <Heading variant="PrimaryTitle">O que é Fator Rh?</Heading>
                 <Text variant="Paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ex debitis ipsa perferendis id, totam eum eos laborum asperiores pariatur excepturi voluptas harum neque recusandae sunt ducimus corporis possimus. Ipsa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus commodi alias aspernatur architecto, eveniet modi nam deleniti minima impedit atque quas illum repudiandae quisquam excepturi autem culpa dolore velit fugiat? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe exercitationem perferendis, sequi deserunt, nam repellendus repudiandae quae dolorum, vel reprehenderit harum fugiat odio odit asperiores nesciunt itaque accusamus molestiae accusantium?</Text>
@@ -23,13 +23,12 @@ export const FatorRh = () => {
                 <Heading variant="PrimaryTitle">Como ele pode influenciar transfusões de sangue é a saúde durante a gravidez? </Heading>
                 <Text variant="Paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae quidem, debitis quos impedit reprehenderit nam quod distinctio rerum veniam quam alias repellendus ut vero quo. Odit neque saepe libero a. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores animi sit, repellendus aliquam ipsum rem veritatis voluptas deleniti, unde aspernatur sequi asperiores numquam et ab recusandae voluptatum porro ea hic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur autem obcaecati assumenda. A libero vitae ullam corporis? Incidunt nam ea suscipit facere enim quaerat nesciunt sint. Nisi similique hic aliquid.</Text>
             </LayoutBaseModify>
-            <BiographyFatorRh Url="../public/img/KarlLandsteiner.jpg" PosicaoDoLado={true}/>
-            <BiographyFatorRh Url="../public/img/KarlLandsteiner.jpg" PosicaoDoLado={false}/>
+            <BiographyFatorRh Name="Karl Landsteiner" Url="../public/img/KarlLandsteiner.jpg" PosicaoDoLado={true}/>
+            <BiographyFatorRh Name="Alexander S. Wiener" Url="../public/img/Wiener.png" PosicaoDoLado={false}/>
             
-                <CardImageSection/>
-                <Divider width='90%'height='2px' backgroundColor='#fff' margin='0 auto' marginTop='20px'/>
-                <VideoCardSection/>
-                
+            <CardImageSection/>
+            <Divider width='90%'height='2px' backgroundColor='#fff' margin='0 auto' marginTop='20px'/>
+            <VideoCardSection/>
             
         </>
        
