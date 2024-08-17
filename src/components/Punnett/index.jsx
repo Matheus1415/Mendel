010c6d8275b4c151e3469be37cd9607475be399d
mps dev aretado <mps.web.tech@gmail.com>
2024-08-17 17:04:15 -0300

    cLONANDO REPOSITORIO

--- src/components/Punnett/index.jsx
+++ src/components/Punnett/index.jsx
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import { CaracteristicaProvider } from '../../contexts/CaracteristicaContext';
 import { PunerContainer } from './PunnerContainer';
-import { usePunnettSquare } from '../../hook/usePunnettSquare';
 
-export const PunerAll = ({title,configAlelo,inputTrue}) => {
-title="Mendel";
-inputTrue=true;
+export const Punnett = ({title = "Mendel",configAlelo,inputTrue = true}) => {
 let configExampleAllelo = [['a','a'],['a','A']];
   // const { combinationsAlelo} = usePunnettSquare(['a','A','b','b'],['a','a','b','b']);
   // console.log(combinationsAlelo)
   return (
     <CaracteristicaProvider>
-      <PunerContainer title={title} configAlelo={configAlelo} inputTrue={inputTrue}/>
+      <PunnettContainer title={title} configAlelo={configAlelo} inputTrue={inputTrue}/>
     </CaracteristicaProvider>
   );
 };
 
-export default PunerAll;
+export default punnett;
