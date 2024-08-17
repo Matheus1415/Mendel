96fa15d183389062d8f53a8f43312fa516754620
AlessandroSilvaS <alessandrosoares1409@gmail.com>
2024-08-17 19:24:25 -0300

    17/08/2024
    Continuação do processo de layout da seção dos exames do fator rh

--- src/pages/FatorRh/ExamFactorRh.jsx
+++ src/pages/FatorRh/ExamFactorRh.jsx
@@ -2,20 +2,28 @@ import {Box, Grid, Heading, Text} from '@chakra-ui/react'
 import ExamRh from '../../data/ExamRh.json'
 export function ExamFactorRh(){
 
+    function alingTexts(indexOfArray){
+        if(indexOfArray%2 === 0){
+            return 'left'
+        }else{
+            return 'right'
+        }
+    }
 
  function showContentBoxes(){
-    return ExamRh.map((Value) => (
-        <Box>
-            <Heading color='#fff'>{Value.title}</Heading>
-            <Text>{Value.firstParagraphy}</Text>
-            <Text>{Value.secondParagrapy}</Text>
+    
+    return ExamRh.map((Value, index) => (
+        <Box >
+            <Heading color='#fff' fontSize='30px' fontWeight='275' maxW='350px' textAlign={alingTexts(index)}>{Value.title}</Heading>
+            <Text fontSize='17px' fontWeight='275' color='#FFFFFF' maxW='350px' textAlign='justify'>{Value.firstParagraphy}</Text>
+            <Text fontSize='17px' fontWeight='275' color='#FFFFFF' maxW='350px' textAlign='justify'>{Value.secondParagrapy}</Text>
         </Box>
     ))
  }
 
     return(
         <Box>
-            <Heading>EXAME DO FATOR RH</Heading>
+            <Heading color='white' fontSize={40} textAlign='center' fontWeight={275} bgGradient='linear(to-r, #0D606780, #19BFCD80)' bgClip='text' marginBottom='100px' marginTop='70px'>EXAME DO FATOR RH</Heading>
             <Grid>
                 {showContentBoxes()}
             </Grid>
