8254cc59acaa4992a96461ca430979a69aea6dde
AlessandroSilvaS <alessandrosoares1409@gmail.com>
2024-08-19 13:02:07 -0300

    19/08/2024
    gap dos elementos

--- src/pages/FatorRh/ExamFactorRh.jsx
+++ src/pages/FatorRh/ExamFactorRh.jsx
@@ -13,8 +13,8 @@ export function ExamFactorRh(){
  function showContentBoxes(){
     
     return ExamRh.map((Value, index) => (
-        <Box >
-            <Heading color='#fff' fontSize='30px' fontWeight='275' maxW='350px' textAlign={alingTexts(index)}>{Value.title}</Heading>
+        <Box marginBottom='40px'>
+            <Heading color='#fff' fontSize='30px' fontWeight='275' maxW='350px' marginBottom='' textAlign={alingTexts(index) }>{Value.title}</Heading>
             <Text fontSize='17px' fontWeight='275' color='#FFFFFF' maxW='350px' textAlign='justify'>{Value.firstParagraphy}</Text>
             <Text fontSize='17px' fontWeight='275' color='#FFFFFF' maxW='350px' textAlign='justify'>{Value.secondParagrapy}</Text>
         </Box>
@@ -24,7 +24,7 @@ export function ExamFactorRh(){
     return(
         <Box>
             <Heading color='white' fontSize={40} textAlign='center' fontWeight={275} bgGradient='linear(to-r, #0D606780, #19BFCD80)' bgClip='text' marginBottom='100px' marginTop='70px'>EXAME DO FATOR RH</Heading>
-            <Grid>
+            <Grid templateColumns={'repeat(2, 370px)'} gap='100px' >
                 {showContentBoxes()}
             </Grid>
         </Box>
