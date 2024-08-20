03bcb1c241a3c90e6be063bdb5d1174ff230b0e8
AlessandroSilvaS <alessandrosoares1409@gmail.com>
2024-08-20 16:28:44 -0300

    20/08/2024

--- src/pages/FatorRh/ExamFactorRh.jsx
+++ src/pages/FatorRh/ExamFactorRh.jsx
@@ -26,7 +26,7 @@ export function ExamFactorRh(){
     return(
         <Box>
             <Heading color='white' fontSize={40} textAlign='center' fontWeight={275} bgGradient='linear(to-r, #0D606780, #19BFCD80)' bgClip='text' marginTop='70px' marginBottom='100px'>EXAME DO FATOR RH</Heading>
-            <Grid templateColumns={'repeat(2, 400px)'} margin='0 auto' width='1000px' gap='230px' bg='red'>
+            <Grid templateColumns={'repeat(2, 400px)'} margin='0 auto' width='1000px' gap='230px'>
                 <GridItem>
                 {showContentBoxes()[0]}
                 {showContentBoxes()[2]}
