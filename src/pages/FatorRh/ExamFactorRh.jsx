e2409a6c165d29a2b7ebc50ec15bf2510844fd1c
AlessandroSilvaS <alessandrosoares1409@gmail.com>
2024-08-21 18:28:55 -0300

    21/08/2024
    Finalização das partes textuais. Faltando apenas a responsividades

--- src/pages/FatorRh/ExamFactorRh.jsx
+++ src/pages/FatorRh/ExamFactorRh.jsx
@@ -1,6 +1,8 @@
-import {Box, Flex, Grid, GridItem, Heading, Text} from '@chakra-ui/react'
+import {Box, Heading, Text, useBreakpointValue} from '@chakra-ui/react'
 import ExamRh from '../../data/ExamRh.json'
 export function ExamFactorRh(){
+
+    const displayForContentBox = useBreakpointValue({base: 'column', sm: 'column', md: 'flex', lg:'flex', xl:'flex'})
 
     function styleTexts(indexOfArray, valueReturnTrue, valueReturnFalse){
         if(indexOfArray%2 === 0){
@@ -11,12 +13,13 @@ export function ExamFactorRh(){
     }
 
  function showContentBoxes(){
-    
+
+
     return ExamRh.map((Value, index) => (
         <Box marginBottom='100px' key={Value.title} textAlign='center' >
-            <Heading color='#fff' fontSize='30px' fontWeight='275'  marginBottom='40px' textAlign={styleTexts(index, 'left', 'right') }>{Value.title}</Heading>
-            <Text fontSize='17px' fontWeight='275' color='#FFFFFF'  textAlign='justify' marginBottom='40px'>{Value.firstParagraphy}</Text>
-            <Text fontSize='17px' fontWeight='275' color='#FFFFFF'  textAlign='justify'>{Value.secondParagrapy}</Text>
+            <Heading color='#fff' fontSize='30px' fontWeight='275' maxW='350px' marginBottom='40px' textAlign={styleTexts(index, 'left', 'right') }>{Value.title}</Heading>
+            <Text fontSize='17px' fontWeight='275' color='#FFFFFF' maxW='350px' textAlign='justify' marginBottom='40px'>{Value.firstParagraphy}</Text>
+            <Text fontSize='17px' fontWeight='275' color='#FFFFFF' maxW='350px' textAlign='justify'>{Value.secondParagrapy}</Text>
         </Box>
     ))
  }
@@ -24,8 +27,8 @@ export function ExamFactorRh(){
     return(
         <Box>
             <Heading color='white' fontSize={40} textAlign='center' fontWeight={275} bgGradient='linear(to-r, #0D606780, #19BFCD80)' bgClip='text' marginTop='70px' marginBottom='100px'>EXAME DO FATOR RH</Heading>
-            <Flex  margin='0 auto' width='1110px' justifyContent='space-between'>
-                <Box>
+            <Box display={displayForContentBox} margin='0 auto' width='1110px' justifyContent='space-between'>
+                <Box textAlign='center'>
                 {showContentBoxes()[0]}
                 {showContentBoxes()[2]}
                 </Box>
@@ -33,7 +36,7 @@ export function ExamFactorRh(){
                 {showContentBoxes()[1]}
                 {showContentBoxes()[3]}
                 </Box>
-            </Flex>
+            </Box>
         </Box>
     )
 }
