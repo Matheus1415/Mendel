c8c0159307755a6e60fb7efa2a2aa6c944e38d49
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-08-25 12:18:31 -0300

    estilização nova de layout

--- src/pages/FatorRh/BiographyFatorRh.jsx
+++ src/pages/FatorRh/BiographyFatorRh.jsx
@@ -1,80 +1,85 @@
-import {Heading, Text, Flex, Box, useBreakpointValue, Image} from '@chakra-ui/react';
+import {Heading, Flex, Box, useBreakpointValue, Text, Image} from '@chakra-ui/react';
 
-const BiographyFatorRh = ({
-    title,
-    isRowReverse,
-    isImage = true,
-    isImageSrc = '../public/img/DNA.png',
-}) => {
-
-    const flexDirection = useBreakpointValue({ base: "column", sm: "column", md: isRowReverse?'row-reverse':'row' });
+const BiographyFatorRh = () => {
+    const flexDirection = useBreakpointValue({ base: "column", sm: "column", md: "row" });
     const contentWidth = useBreakpointValue({  base: "100%",  sm: "100%", md: '50%'});
     const flexAlignContent = useBreakpointValue({  base: "center",  sm: "center", md: ''});
-    
-    const styleText = {
-        fontSize: '17px',
-        fontWeight: 275,
-        lineHeight: '25.5px',
-        textAlign: 'justify'
-    }
-    const styleHeading = {
-        fontSize: '40px',
-        textAlign: 'left',
-        fontWeight: 275,
-    }
 
     return(
-
+        
         <Box>
             <Flex
-            mx="auto" 
-            maxW="1300px" 
-            p={8} 
-            gap={6} 
-            direction={flexDirection} 
-            justifyContent="space-between"
-            alignItems="center"
+                mb="100px"
+                mx="auto"
+                maxW="1300px"
+                p={8}
+                gap={6}
+                direction={ flexDirection }
+                justifyContent="space-between"
             >
-            {isImage && <Image w="lg" borderRadius="2xl" objectFit="cover" src={isImageSrc} alt="Estatua de Mendel" />
-                    }
                 <Flex
-                direction="column" 
-                justifyContent="flex-start" 
-                h="100%" w="100%" 
-                maxW={contentWidth} 
-                alignItems={flexAlignContent}
+                    direction="column"
+                    justifyContent="flex-start"
+                    maxW={ contentWidth }
+                    h="100%"
+                    w="100%"
+                    alignItems={ flexAlignContent }
                 >
                     <Box>
-                        <Heading
-                        variant="SecondTitle"
-                        sx={styleHeading}> {title} </Heading>
-                        <Text
-                        variant="Paragraph"
-                        sx={styleText}
-                        >
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum aut hic libero quasi expedita animi temporibus consequuntur! Ducimus libero a provident aperiam, excepturi voluptate, est ad molestias, velit obcaecati facere.
+                        <Heading  variant="SecondTitle">Karl Landsteiner</Heading>
+                        <Text variant="Paragraph">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laborum facere, nobis sit libero ea voluptatem ducimus cupiditate quas vel nihil voluptatum ad, aliquid ipsa. Laborum sunt unde eius et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, praesentium sit. Ratione, quisquam? Quo asperiores beatae voluptatum? Deserunt, aliquid debitis voluptatem, quam sunt minima ipsa aperiam perspiciatis voluptas, molestias reprehenderit.
                         </Text>
-                        <Text
-                        variant="Paragraph"
-                        sx={styleText}
-                        >
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum aut hic libero quasi expedita animi temporibus consequuntur! Ducimus libero a provident aperiam, excepturi voluptate, est ad molestias, velit obcaecati facere.
+
+                        <Text variant="Paragraph">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laborum facere, nobis sit libero ea voluptatem ducimus cupiditate quas vel nihil voluptatum ad, aliquid ipsa. Laborum sunt unde eius et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, praesentium sit. Ratione, quisquam? Quo asperiores beatae voluptatum? Deserunt, aliquid debitis voluptatem, quam sunt minima ipsa aperiam perspiciatis voluptas, molestias reprehenderit.
                         </Text>
-                        <Text 
-                        variant="Paragraph"
-                        sx={styleText}
-                        >
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum aut hic libero quasi expedita animi temporibus consequuntur! Ducimus libero a provident aperiam, excepturi voluptate, est ad molestias, velit obcaecati facere.
+
+                        <Text variant="Paragraph">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laborum facere, nobis sit libero ea voluptatem ducimus cupiditate quas vel nihil voluptatum ad, aliquid ipsa. Laborum sunt unde eius et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, praesentium sit. Ratione, quisquam? Quo asperiores beatae voluptatum? Deserunt, aliquid debitis voluptatem, quam sunt minima ipsa aperiam perspiciatis voluptas, molestias reprehenderit.
                         </Text>
-                        <Text 
-                        variant="Paragraph"
-                        sx={styleText}
-                        >
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum aut hic libero quasi expedita animi temporibus consequuntur! Ducimus libero a provident aperiam, excepturi voluptate, est ad molestias, velit obcaecati facere.
-                        </Text>
-                    </Box>    
+                    </Box>
                 </Flex>
+                    <Image w="lg" borderRadius="2xl" objectFit="cover" src="./public/img/KarlLandsteiner.jpg" alt="KarlLandsteiner" />
             </Flex>
+    
+            <Flex
+                mx="auto"
+                maxW="1300px"
+                p={8}
+                gap={6}
+                direction={ flexDirection }
+                justifyContent="space-between"
+            >
+                <Flex
+                    direction="column"
+                    justifyContent="flex-start"
+                    maxW={ contentWidth }
+                    h="100%"
+                    w="100%"
+                    alignItems={ flexAlignContent }
+                >
+                    <Box>
+                        <Heading  variant="SecondTitle"> Alexander Solomon Wiener</Heading>
+                        <Text variant="Paragraph">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laborum facere, nobis sit libero ea voluptatem ducimus cupiditate quas vel nihil voluptatum ad, aliquid ipsa. Laborum sunt unde eius et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, praesentium sit. Ratione, quisquam? Quo asperiores beatae voluptatum? Deserunt, aliquid debitis voluptatem, quam sunt minima ipsa aperiam perspiciatis voluptas, molestias reprehenderit.
+                        </Text>
+
+                        <Text variant="Paragraph">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laborum facere, nobis sit libero ea voluptatem ducimus cupiditate quas vel nihil voluptatum ad, aliquid ipsa. Laborum sunt unde eius et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, praesentium sit. Ratione, quisquam? Quo asperiores beatae voluptatum? Deserunt, aliquid debitis voluptatem, quam sunt minima ipsa aperiam perspiciatis voluptas, molestias reprehenderit.
+                        </Text>
+
+                        <Text variant="Paragraph">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laborum facere, nobis sit libero ea voluptatem ducimus cupiditate quas vel nihil voluptatum ad, aliquid ipsa. Laborum sunt unde eius et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, praesentium sit. Ratione, quisquam? Quo asperiores beatae voluptatum? Deserunt, aliquid debitis voluptatem, quam sunt minima ipsa aperiam perspiciatis voluptas, molestias reprehenderit.
+                        </Text>
+                    </Box>
+                </Flex>
+                    <Image w="lg" borderRadius="2xl" objectFit="cover" src="./public/img/Wiener.png" alt="Wiener" />
+            </Flex>
+
+
+
+
         </Box>
              
        
--- src/pages/FatorRh/index.jsx
+++ src/pages/FatorRh/index.jsx
@@ -1,134 +1,19 @@
-import { Flex ,Text, Heading, Box, useBreakpointValue, Image } from '@chakra-ui/react';
 import { LayoutIntroduction } from '../../layout/LayoutIntroduction';
+import WantFatorRh from './WantFatorRh';
 import BiographyFatorRh  from './BiographyFatorRh';
 import { TableSection } from './TableSection';
 import { ExamFactorRh } from './ExamFactorRh';
 
 
-export const FatorRh = ({
-    isImage = true,
-    isRowReverse = true, 
-    isImageSrc = '../public/img/DNA.png',
-    backgroundImage = '../public/img/background4.png'
-}) => {
-
-    const flexDirection = useBreakpointValue({ base: "column", sm: "column", md: isRowReverse?'row-reverse':'row' });
-    const contentWidth = useBreakpointValue({  base: "100%",  sm: "100%", md: '50%'});
-    const flexAlignContent = useBreakpointValue({  base: "center",  sm: "center", md: ''});
-    
-    const styleText = {
-        fontSize: '17px',
-        fontWeight: 275,
-        lineHeight: '25.5px',
-        textAlign: 'justify'
-    }
-    const styleHeading = {
-        fontSize: '40px',
-        textAlign: 'right',
-        fontWeight: 275,
-        bgGradient: 'linear(to-r, #0D066780, #19BFCD80)',
-        bgClip: 'text'
-    }
-
-
+export const FatorRh = () => {
 
     return(
         <>
             <LayoutIntroduction query="Fator Rh">Fator Rh</LayoutIntroduction>
-
-
-
-
-            <Box
-            backgroundRepeat="repeat"
-            backgroundSize="cover" 
-            backgroundPosition="center" 
-            backgroundImage={`url(${backgroundImage})`}
-            >
-                <Flex
-                mx="auto" 
-                maxW="1300px" 
-                p={8} 
-                gap={6} 
-                direction={flexDirection} 
-                justifyContent="space-between"
-                alignItems="center"
-                >
-                    <Flex
-                    direction="column" 
-                    justifyContent="flex-start" 
-                    h="100%" w="100%" 
-                    maxW={contentWidth} 
-                    alignItems={flexAlignContent}
-                    >
-                        <Heading 
-                        variant="PrimaryTitle"
-                        sx={styleHeading}
-                        >O que é</Heading>
-                        <Text 
-                        variant="Paragraph"
-                        sx={styleText}
-                        >
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolorem autem porro aperiam enim natus, nemo labore hic. Asperiores eveniet facilis excepturi consequuntur nesciunt laudantium harum voluptate totam corrupti odio.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit adipisci harum maiores non corporis aliquam minus, repudiandae, sapiente accusamus autem ad excepturi vitae quaerat quo perspiciatis facere tempora maxime facilis.
-                        </Text>
-                        <Text
-                        variant="Paragraph"
-                        sx={styleText}
-                        >
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi laudantium inventore et aliquid! Fugiat adipisci quis reiciendis. Facere beatae rerum ratione ea! Vel nemo officiis sint, dolore molestiae error libero?
-                        </Text>
-                        <Heading
-                        variant="PrimaryTitle"
-                        sx={styleHeading}
-                        >
-                            Importância
-                        </Heading>
-                        <Text 
-                        variant="Paragraph"
-                        sx={styleText}
-                        >
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolorem autem porro aperiam enim natus, nemo labore hic. Asperiores eveniet facilis excepturi consequuntur nesciunt laudantium harum voluptate totam corrupti odio.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit adipisci harum maiores non corporis aliquam minus, repudiandae, sapiente accusamus autem ad excepturi vitae quaerat quo perspiciatis facere tempora maxime facilis.
-                        </Text>
-                        <Text
-                        variant="Paragraph"
-                        sx={styleText}
-                        >
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi laudantium inventore et aliquid! Fugiat adipisci quis reiciendis. Facere beatae rerum ratione ea! Vel nemo officiis sint, dolore molestiae error libero?
-                        </Text>
-                    </Flex>
-                    {
-                    isImage && 
-                    <Image 
-                    w="lg"
-                    ml={{ base: '0', md: '-100px' }}
-                    borderRadius="2xl"
-                    objectFit="cover"
-                    src={isImageSrc}
-                    alt="Imagem de fundo sobre DNA" 
-                    />
-                    }
-                </Flex>
-                <Heading
-                fontSize="40px"
-                lineHeight="60px"
-                textAlign="center"
-                variant="PrimaryTitle"
-                fontWeight= {275}
-                bgGradient= 'linear(to-r, #0D066780, #19BFCD80)'
-                alignItems= 'justify-content'
-                bgClip= 'text'
-                
-                >Criadores</Heading>
-                <BiographyFatorRh title="Karl Landsteiner" isImageSrc="../public/img/KarlLandsteiner.jpg" isRowReverse={true}/>
-                <br />
-                <BiographyFatorRh title="Alexander S. Wiener" isImageSrc="../public/img/Wiener.png" isRowReverse={true}/>
-                <TableSection/>
-                <ExamFactorRh/>
-            </Box>
-            
-            
+            <WantFatorRh/>
+            <BiographyFatorRh/>
+            <TableSection/>
+            <ExamFactorRh/>  
         </>
        
     );
