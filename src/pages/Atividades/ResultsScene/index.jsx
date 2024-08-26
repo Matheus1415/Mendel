17547ffd13f3da347afd2b6880f035b206c78f6d
Gabriel De Sousa <156588121+Gabriel-Sousa-Dev@users.noreply.github.com>
2024-08-25 22:52:02 -0300

    Update index.jsx

--- src/pages/Atividades/ResultsScene/index.jsx
+++ src/pages/Atividades/ResultsScene/index.jsx
@@ -1,17 +1,44 @@
-import { Box, Divider, Flex, Heading } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, Heading, OrderedList } from "@chakra-ui/react";
+import { QuestionHistory } from "../questionHistory";
 
 export function ResultsScene({resultState}){
-    console.log('res', resultState);
-    
+    // 675 262
     return (
-        <Flex height='750px' w='full' justifyContent='space-around'>
-            asaa
-            <Box>
-                <Heading variant='PrimaryTitle'>asas</Heading>
+        <Flex height='750px' w='full' justifyContent='space-around' gap='0.75rem'>
+            <Box flex='2.5' maxWidth='70%'>
+                <OrderedList>
+                {
+                    resultState.userResponses.map( response => {
+                        
+                        return (<QuestionHistory
+                            response={response}
+                            questionType={response.questionType}
+                        />)
+                        
+                    } )
+                }
+                </OrderedList>
             </Box>
             <Divider orientation="vertical" colorScheme='blue' />
-            <Box>
-                <Heading variant='PrimaryTitle'>oie</Heading>
+
+            <Box flex='1' flexShrink='0' display='flex' flexDirection='column' justifyContent='space-between'>
+                <Heading variant='SecondTitle' textAlign='center'>Porcentagem de Acertos</Heading>
+                
+                <Flex direction='column'>
+                    <Heading variant='PrimaryTitle' textAlign='center' fontSize='2.75rem'>Nota: 7,0</Heading>
+                    <Heading variant='SecondTitle' textAlign='center' fontSize='2rem'>Não esqueça de ler as questões com calma!</Heading>
+                </Flex>
+
+                <Flex direction='column'>
+                    <Heading variant='SecondTitle' fontSize='2rem' >Múltiplas: 45%</Heading>
+                    <Heading variant='SecondTitle' fontSize='2rem' >Únicas: 70%</Heading>
+                    <Heading variant='SecondTitle' fontSize='2rem' >Digitaveis: 25%</Heading>    
+                </Flex>  
+
+                <Flex justifyContent='space-between'>
+                    <Button variant='nextPage' >Voltar</Button> 
+                    <Button variant='nextPage' >Atividade</Button>
+                </Flex>
             </Box>
         </Flex>
     )
