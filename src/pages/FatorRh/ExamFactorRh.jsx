99051191a938e09cb6940c044964e161a8f7716b
AlessandroSilvaS <alessandrosoares1409@gmail.com>
2024-08-25 22:43:49 -0300

    formatação dos códigos

--- src/pages/FatorRh/ExamFactorRh.jsx
+++ src/pages/FatorRh/ExamFactorRh.jsx
@@ -14,8 +14,6 @@ export function ExamFactorRh(){
     }
 
  function showContentBoxes(){
-
-
     return ExamRh.map((Value, index) => (
         <Box marginBottom='100px' key={Value.title} textAlign='center' >
             <Heading color='#fff' fontSize='30px' fontWeight='275' maxW='350px' marginBottom='40px' textAlign={styleTexts(index, 'left', 'right') }>{Value.title}</Heading>
@@ -24,18 +22,17 @@ export function ExamFactorRh(){
         </Box>
     ))
  }
-
     return(
         <Box>
             <Heading color='white' fontSize={40} textAlign='center' fontWeight={275} bgGradient='linear(to-r, #0D606780, #19BFCD80)' bgClip='text' marginTop='70px' marginBottom='100px'>EXAME DO FATOR RH</Heading>
             <Box display={displayForContentBox} margin='0 auto' width={widthOfBoxeContent} justifyContent='space-between'>
                 <Box >
-                {showContentBoxes()[0]}
-                {showContentBoxes()[2]}
+                    {showContentBoxes()[0]}
+                    {showContentBoxes()[2]}
                 </Box>
                 <Box>
-                {showContentBoxes()[1]}
-                {showContentBoxes()[3]}
+                    {showContentBoxes()[1]}
+                    {showContentBoxes()[3]}
                 </Box>
             </Box>
         </Box>
