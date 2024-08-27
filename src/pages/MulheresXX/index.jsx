dacba47812fd3673885be69ce32c80bcdf69bb21
Jeferson Bryan <jefersonbryan1307@gmail.com>
2024-08-27 15:31:39 -0300

    commit test

--- src/pages/MulheresXX/index.jsx
+++ src/pages/MulheresXX/index.jsx
@@ -1,4 +1,4 @@
-import { Heading, Highlight, Flex, Text, Box } from "@chakra-ui/react";
+import { Heading, Highlight, Flex, Text, Box, Image } from "@chakra-ui/react";
 import VLibras from "@djpfs/react-vlibras";
 
 export function MulheresXX() {
@@ -33,25 +33,20 @@ export function MulheresXX() {
       <Flex
         direction={{ base: "column", md: "row" }}
         p={[3, 4, 8]}
-        maxW="1300px"
+        width="70%"
         mx="auto"
         alignItems="flex-start"
         gap={6}
         justify="center"
         position="relative"
         overflow="hidden"
+        justifyContent="space-between"
       >
-        <Box
-          position="absolute"
-          right="800"
-          top="0"
-          h="100%"
-          w="60%"
-          backgroundImage="url('/assets/DNA.png')"
-          backgroundPosition="left"
-          backgroundRepeat="no-repeat"
-          opacity="0.7"
-        />
+        <Box>
+         <Image src ="../../../public/assets/DNA.png"
+         />
+        </Box>
+        
 
         <Box
           flex="1"
@@ -62,6 +57,7 @@ export function MulheresXX() {
           textAlign="justify"
           p={4}
           zIndex="1"
+          maxWidth="500px"
         >
           <Heading
             variant="PrimaryTitle"
@@ -72,52 +68,18 @@ export function MulheresXX() {
             Importância
           </Heading>
           <Text variant="Paragraph">
-            A ciência genética, enquanto uma das disciplinas mais centrais na
-            esfera da biologia, tem sido crucial para o aprofundamento de nossa
-            compreensão da hereditariedade e dos processos vitais que regem a
-            vida. O campo da genética não se restringe apenas à análise dos
-            padrões de herança, mas também se estende à compreensão dos
-            mecanismos moleculares que fundamentam a expressão e a variação
-            genética. Nesse contexto, as mulheres desempenharam um papel
-            fundamental, sendo protagonistas em muitas das descobertas e avanços
-            que moldaram a genética moderna. Desde os primórdios da genética, as
-            mulheres estiveram na vanguarda de explorações científicas que
-            desafiaram o conhecimento estabelecido e expandiram as fronteiras do
-            que era possível compreender sobre a biologia dos organismos. O
-            impacto de suas contribuições é notável não apenas pelo volume de
-            descobertas inovadoras, mas também pela profundidade e pela
-            complexidade dessas descobertas. As mulheres científicas não apenas
-            se destacaram por suas investigações pioneiras, mas também por suas
-            habilidades analíticas e criativas, que têm sido essenciais para o
-            desenvolvimento de teorias e tecnologias genéticas.
-            <br />
-            Por exemplo, a observação meticulosa e a análise crítica realizadas
-            por cientistas como Rosalind Franklin, com suas imagens de raios X
-            do DNA, foram fundamentais para a elucidação da estrutura em dupla
-            hélice da molécula. Esse marco transformou radicalmente nossa
-            compreensão da hereditariedade, confirmando teorias existentes e
-            revelando novas dimensões sobre a estrutura e a função do DNA. Essas
-            descobertas forneceram a base para uma série de avanços subsequentes
-            na biologia molecular. No cenário mais contemporâneo, as
-            contribuições de mulheres como Jennifer Doudna e Emmanuelle
-            Charpentier com o desenvolvimento da tecnologia de edição genética
-            CRISPR-Cas9 representam um salto paradigmático no campo. A
-            capacidade de manipular o material genético com precisão tem
-            implicações vastas e profundas, desde o tratamento de doenças
-            genéticas até a modificação de características de organismos para
-            melhorar a saúde e a agricultura. O trabalho dessas cientistas não
-            só expandiu nosso arsenal de ferramentas genéticas, mas também
-            reconfigurou o horizonte da pesquisa genética e das aplicações
-            clínicas.
-            <br />
-            Portanto, ao refletirmos sobre a evolução da ciência genética, é
-            imperativo reconhecer e valorizar as contribuições das mulheres,
-            cuja influência foi e continua a ser inestimável para o progresso da
-            disciplina. Seus esforços incansáveis e suas inovações notáveis não
-            apenas enriqueceram nosso conhecimento, mas também estabeleceram
-            novos paradigmas para a investigação científica e a prática médica.
-            O legado das mulheres na genética é uma testemunha da importância de
-            sua presença e impacto na construção da ciência moderna.
+      
+
+A ciência genética é crucial para entender a hereditariedade e os mecanismos moleculares da vida. As mulheres desempenharam papéis fundamentais nesse campo, contribuindo significativamente para suas descobertas e avanços. Desde o início da genética, elas foram pioneiras em desafios ao conhecimento estabelecido e na expansão da biologia.
+<br />
+Rosalind Franklin, com suas imagens de raios X do DNA, revelou a estrutura em dupla hélice da molécula, transformando a compreensão da hereditariedade e confirmando teorias existentes. Recentemente, Jennifer Doudna e Emmanuelle Charpentier avançaram com a tecnologia de edição genética CRISPR-Cas9, permitindo manipulação precisa do DNA. Essa inovação tem vastas implicações para tratar doenças genéticas e melhorar saúde e agricultura.
+<br />
+Essas contribuições demonstram não apenas investigações pioneiras, mas também habilidades analíticas e criativas essenciais para o desenvolvimento da genética. Reconhecer o impacto das mulheres na genética é vital, pois seu trabalho é crucial para o progresso da ciência e da prática médica moderna. O legado feminino na genética reflete a importância de sua presença e impacto na ciência contemporânea.
+
+
+
+
+
           </Text>
         </Box>
       </Flex>
