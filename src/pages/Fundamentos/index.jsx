4492c44efcfa2bc39b9009ed99d42de4c3ee0626
Thiago Ferreira da Silva <thiagocraft510@gmail.com>
2024-08-27 15:30:58 -0300

    Erro da ToolTip resolvida, melhorias na indentação do codigo e área de interatividade do Centõmetro do cromossmo adcionada

--- src/pages/Fundamentos/index.jsx
+++ src/pages/Fundamentos/index.jsx
@@ -1,14 +1,10 @@
+import React from 'react';
 import { Box, Flex, Heading, Image, Text, UnorderedList, ListItem, Button, Tooltip } from '@chakra-ui/react';
 import { LayoutIntroduction } from '../../layout/LayoutIntroduction';
-import React from 'react';
 import { LayoutBaseModify } from '../../layout/LayoutBaseModify';
 
 
 export const Fundamentos = () => {
-    // Function to handle hover events
-    const telomenoOver = () => {
-        console.log("Mouse Over Telômero"); // Log to console on hover
-    };
 
 return (
 
@@ -40,7 +36,7 @@ FUNDAMENTOS DA GENÉTICA
           gap="2.25rem"
           maxWidth="25.5rem"
         >
-            <Heading color="Primary" variant="SecondTitle">
+            <Heading variant="SecondTitle">
                 CÉLULA DIPLOIDE
             </Heading>
             <Image src='/assets/diploid-cell.png' alt="Célula Diploide" />
@@ -59,11 +55,11 @@ FUNDAMENTOS DA GENÉTICA
           gap="2.25rem"
           maxWidth="25.5rem"
         >
-            <Heading color="Primary" variant="SecondTitle">
+            <Heading variant="SecondTitle">
                 CÉLULA HAPLOIDE
             </Heading>
             <Image src='/assets/haploid-cell.png' alt="Célula Haploide" />
-            <UnorderedList textAlign="justify" color="Primary" fontSize="17px" fontWeight="275">
+            <UnorderedList textAlign="justify" color='Primary' fontSize="17px" fontWeight="275">
                 <ListItem>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</ListItem>
                 <ListItem>When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</ListItem>
                 <ListItem>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</ListItem>
@@ -74,107 +70,114 @@ FUNDAMENTOS DA GENÉTICA
 
 {/* Section for Chromosomes */}
 <Box as='section' mb='10rem'>
-    <Heading variant="PrimaryTitle" textAlign="center" mb="5rem">
-    CROMOSSOMOS
-    </Heading>
-    <Flex
-      textAlign='center'
-      alignItems='center'
-      justifyContent='space-around'
-      direction={{base:'column-reverse', lg:'row'}}
+  <Heading variant="PrimaryTitle" textAlign="center" mb="5rem">
+  CROMOSSOMOS
+  </Heading>
+  <Flex
+    textAlign='center'
+    alignItems='center'
+    justifyContent='space-around'
+    direction={{base:'column-reverse', lg:'row'}}
+  >
+    <Box
+      maxWidth={{base:'55rem', lg:'25rem'}}
+      marginTop='2rem'
     >
-        <Box
-          maxWidth={{base:'55rem', lg:'25rem'}}
-          marginTop='2rem'
-        >
-            <Text variant="Paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Text>
-            <Text variant="Paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</Text>
+      <Text variant="Paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Text>
+      <Text variant="Paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</Text>
             
-            <Text variant="Paragraph">CROMOSSOMOS HOMÓLOGOS: Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in classical Latin literature from 45 BC, making it over 2000 years.</Text>
-            <Text variant="Paragraph">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</Text>
-        </Box>
+      <Text variant="Paragraph">CROMOSSOMOS HOMÓLOGOS: Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in classical Latin literature from 45 BC, making it over 2000 years.</Text>
+      <Text variant="Paragraph">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</Text>
+    </Box>
 
-        {/* Box for Chromosome Image with Highlighted Areas */}
-        <Box 
-          maxHeight='29rem' 
-          maxWidth='30rem' 
-          position='relative'
-        >
-            <Image src='/assets/cromossome-image.png' alt="Cromossomos" />
-            <Tooltip label='placeholder1' hasArrow placement='right' w='fit-content'>
-                <Box
-                  bg='red.500'
-                  opacity='0.4'
-                  h='13.7%'
-                  w='13%'
-                  borderRadius='40%'
-                  position='absolute'
-                  top='2%'
-                  left='0.5%'
-                  onMouseOver={telomenoOver}
-                ></Box>
-            </Tooltip>
-            <Tooltip label='placeholder2' hasArrow placement='right' w='fit-content'>
-                <Box
-                  bg='red.500'
-                  opacity='0.4'
-                  h='14%'
-                  w='13%'
-                  borderRadius='40%'
-                  position='absolute'
-                  top='3%'
-                  left='41%'
-                  onMouseOver={telomenoOver}
-                ></Box>
-            </Tooltip>
-            <Tooltip label='placeholder3' hasArrow placement='right' w='fit-content'>
-                <Box
-                  bg='red.500'
-                  opacity='0.4'
-                  h='15.7%'
-                  w='15%'
-                  borderRadius='40%'
-                  position='absolute'
-                  top='82.5%'
-                  left='1.5%'
-                  onMouseOver={telomenoOver}
-                ></Box>
-            </Tooltip>
-            <Tooltip label='placeholder4' hasArrow placement='right' w='fit-content'>
-                <Box
-                  bg='red.500'
-                  opacity='0.4'
-                  h='16%'
-                  w='14%'
-                  borderRadius='40%'
-                  position='absolute'
-                  top='83%'
-                  left='39.3%'
-                  onMouseOver={telomenoOver}
-                ></Box>
-            </Tooltip>
-        </Box>
-    </Flex>
+    <Box // BOX DE CROMOSSOMOS COM AREAS DE INTERACAO
+      maxHeight='29rem' 
+      maxWidth='30rem' 
+      position='relative'
+    >
+      <Image src='/assets/cromossome-image.png' alt="Cromossomos" />
+      <Tooltip label='Esta parte se denomina Telõmeno' hasArrow placement='top' maxW='fit-content'>
+      <Box // BRAÇO ESQUERDO SUPERIOR DO CROMOSSOMO
+        bg='red.500'
+        opacity='0.4'
+        h='14%'
+        w='12%'
+        borderRadius='40%'
+        position='absolute'
+        top='2%'
+        left='1%'
+      ></Box>
+      </Tooltip>
+      <Tooltip label='Esta parte se denomina Telõmeno' hasArrow placement='top' maxW='fit-content' >
+        <Box // BRAÇO DIREITO SUPERIOR DO CROMOSSOMO
+          bg='red.500'
+          opacity='0.4'
+          h='14%'
+          w='13%'
+          borderRadius='40%'
+          position='absolute'
+          top='3%'
+          left='41%'
+        ></Box>
+      </Tooltip>
+      <Tooltip label='Esta parte se denomina Telõmeno' hasArrow placement='bottom' maxW='fit-content'>
+        <Box // BRAÇO ESQUERDO INFERIOR DO CROMOSSOMO
+          bg='red.500'
+          opacity='0.4'
+          h='15.7%'
+          w='15%'
+          borderRadius='40%'
+          position='absolute'
+          top='82.5%'
+          left='1.5%'
+        ></Box>
+      </Tooltip>
+      <Tooltip label='Esta parte se denomina Telõmeno' hasArrow placement='bottom' maxW='fit-content'>
+        <Box // BRAÇO DIREITO INFERIOR DO CROMOSSOMO
+          bg='red.500'
+          opacity='0.4'
+          h='16%'
+          w='14%'
+          borderRadius='40%'
+          position='absolute'
+          top='83%'
+          left='39.3%'
+        ></Box>
+      </Tooltip>
+      <Tooltip label='Esta parte se denomina Centrõmeno' hasArrow placement='right' maxW='fit-content'>
+      <Box // CENTRÕMENO(O MEIO DO CROMOSSMO)
+          bg='red.500'
+          opacity='0.4'
+          h='14%'
+          w='17%'
+          borderRadius='50%'
+          position='absolute'
+          top='42.5%'
+          left='18.5%'
+        ></Box>
+      </Tooltip>
+    </Box>
+  </Flex>
 </Box>
 
 <LayoutBaseModify
 isRowReverse
 >
-    <Box 
-      maxWidth='25rem' 
-      marginLeft='auto'
-      mb='10rem'
-    >
-        <Heading variant='PrimaryTitle'>DNA</Heading>
-        <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
-        <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
-        <Heading variant='PrimaryTitle'>RNA</Heading>
-        <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
-        <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
-        <Heading variant='PrimaryTitle'>GENE</Heading>
-        <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
-        <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
-    </Box>
+  <Box 
+    maxWidth='25rem' 
+    marginLeft='auto'
+    mb='10rem'
+  >
+    <Heading variant='PrimaryTitle' textAlign='right'>DNA</Heading>
+    <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
+    <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
+    <Heading variant='PrimaryTitle' textAlign='right'>RNA</Heading>
+    <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
+    <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
+    <Heading variant='PrimaryTitle' textAlign='right'>GENE</Heading>
+    <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
+    <Text variant='Paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet doloremque neque esse facere fugit eligendi eos quaerat suscipit at recusandae, pariatur enim non vero asperiores id beatae et assumenda nostrum.</Text>
+  </Box>
 </LayoutBaseModify>
 
 <Flex // SEÇÃO DE ALELOS
@@ -224,7 +227,7 @@ isRowReverse
           textAlign='center'
         
         >
-            DIFERENÇA DE HOMOZIGOTO E HETEROZIGOTO
+          DIFERENÇA DE HOMOZIGOTO E HETEROZIGOTO
         </Heading>
         <Flex 
           justifyContent='space-between' 
@@ -268,10 +271,10 @@ isRowReverse
           gap="2.25rem"
           maxWidth="25.5rem"
         >
-            <Heading color="Primary" variant="SecondTitle">
-                FENÓTIPO
-            </Heading>
-                <Text variant='Paragraph'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text> 
+          <Heading color="Primary" variant="SecondTitle">
+          FENÓTIPO
+          </Heading>
+          <Text variant='Paragraph'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text> 
         </Box>
 
         {/* Box for Haploid Cell */}
@@ -282,16 +285,16 @@ isRowReverse
           gap="2.25rem"
           maxWidth="25.5rem"
         >
-            <Heading color="Primary" variant="SecondTitle">
-                GENÓTIPO
-            </Heading>
-            <Text variant='Paragraph'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+          <Heading color="Primary" variant="SecondTitle">
+          GENÓTIPO
+          </Heading>
+          <Text variant='Paragraph'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
         </Box>
     </Flex>
 
         <Button 
           border='1px solid' 
-          borderColor='#EBEBEB' 
+          borderColor='#EBEBEB'
           colorScheme='#000100' 
           borderRadius='50px' 
           fontWeight='275px' 
@@ -304,8 +307,8 @@ isRowReverse
           transform: 'translateX(10px)',
           transitionDuration: '0.2s',
           transitionTimingFunction: "ease-in-out"
-        }}
-    >Ir para a próxima página</Button>
+          }}
+        >Ir para a próxima página</Button>
     
 </Box>
 
