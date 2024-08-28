df54be8b3c274dc4385f34c12f68363e521343ab
Thiago Ferreira da Silva <thiagocraft510@gmail.com>
2024-08-28 15:00:28 -0300

    Responsividade no LayoutIntroduction.

--- src/layout/LayoutIntroduction.jsx
+++ src/layout/LayoutIntroduction.jsx
@@ -1,9 +1,10 @@
-import {  Flex, Heading, Highlight,  } from "@chakra-ui/react";
+import {  Flex, Heading, Highlight, useBreakpointValue,  } from "@chakra-ui/react";
 
 export const LayoutIntroduction = ({query,children}) => {
+    const textSize = useBreakpointValue({base:"18px", md:"30px", lg:"35px"})
     return(
         <Flex justify='center' align='center' minH='100vh' backgroundImage='assets/banner.jpg' bgRepeat='no-repeat' bgSize='cover'>
-            <Heading variant='PrimaryTitle' color='Primary' textAlign='center' fontSize={[25, 25, 40]} opacity='0.3' fontWeight={30} letterSpacing='wider' >
+            <Heading variant='PrimaryTitle' color='Primary' textAlign='center' fontSize={textSize} opacity='0.3' fontWeight={30} letterSpacing='wider' >
 
                 <Highlight query={query} styles={{fontSize: '3.5em', color: 'Primary', display: 'block', fontWeight:'thin', letterSpacing:'1px'}}>{children}</Highlight>
             </Heading>
